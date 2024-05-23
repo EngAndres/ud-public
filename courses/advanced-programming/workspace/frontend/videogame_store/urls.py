f596023649a8a970ae2bb00a43508ed5f05fb3f0
casierrav <jerarquia04@gmail.com>
2024-05-23 10:06:55 -0500

    Advanced Programming: frontend code of example project

--- courses/advanced-programming/workspace/frontend/videogame_store/urls.py
+++ courses/advanced-programming/workspace/frontend/videogame_store/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='store_home'),
     path('contact/', views.contact, name='store_contact'),
     path('login/', views.login, name='store_login'),
+    path('videogames_list/', views.videogames_list, name='store_videogames_list')
 ]
--- courses/advanced-programming/workspace/frontend/videogame_store/views.py
+++ courses/advanced-programming/workspace/frontend/videogame_store/views.py
@@ -9,3 +9,20 @@ def login(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+def videogames_list(request):
+    videogames_list = [
+        {
+            "name": "CoD",
+            "price": "150.000 COP",
+            "description": "Militia first-person videogame",
+            "image": "images/CoD.jpeg"
+        },
+        {
+            "name": "CupHead",
+            "price": "90.000 COP",
+            "description": "Two sinblings sell their souls to the devil",
+            "image": "images/CupHead.png"
+        }
+    ]
+    return render(request, 'videogames_list.html', {"videogames": videogames_list})
