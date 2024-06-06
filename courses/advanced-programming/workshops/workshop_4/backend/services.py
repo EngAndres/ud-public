65f3bacfc967c5212cb8047da347d12bf4a8773b
casierrav <jerarquia04@gmail.com>
2024-06-06 10:54:10 -0500

    Advanced Programming: example of workshop 4 using web services with FastAPI and MTV with Django

--- courses/advanced-programming/workshops/workshop_4/backend/services.py
+++ courses/advanced-programming/workshops/workshop_4/backend/services.py
@@ -5,18 +5,18 @@ Author: Carlos Andrés Sierra <cavirguezs@udistrital.edu.co>
 """
 
 import os
+from typing import List
 import uvicorn
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
+from pydantic import BaseModel
 
 load_dotenv()
 engine = create_engine(
-    f"postgresql://\
-    {os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}\
-    @{os.getenv('DB_URL')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+    f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_URL')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 )
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -25,10 +25,11 @@ metadata = MetaData()
 products = Table(
     "products",
     metadata,
-    Column("id", Integer, primary_key=True),
+    Column("id", Integer, primary_key=True, autoincrement=True),
     Column("name", String),
-    Column("description", String),
+    Column("description", String, default="No available"),
 )
+metadata.create_all(engine)
 
 app = FastAPI()
 
@@ -49,9 +50,15 @@ def hello_ud():
     """This is a healthcheck service just to validade is backend is up"""
     return "Welcome to UD!"
 
+class Product(BaseModel):
+    id: int = None
+    name: str
+    description: str
+
+
 
 @app.get("/products")
-def get_products():
+def get_products() -> List[Product]:
     """
     This service returns all the products stored
     in the database
@@ -60,17 +67,21 @@ def get_products():
     query = products.select()
     result = session.execute(query)
     products = result.fetchall()
+
     return products
 
 
-@app.post("/products")
-def create_product(name_arg: str, description_arg: str):
+@app.post("/products/add")
+def create_product(product: Product):
     """This method creates a product inside the database"""
-    query = products.insert().values(name=name_arg, description=description_arg)
+    print(product)
+    query = products.insert().values(name=product.name, description=product.description)
     session.execute(query)
     session.commit()
     return {"message": "Product created successfully"}
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    product = Product(name="Product 3", description="This is a product 2")
+    print(create_product(product))
+    uvicorn.run(app, host="0.0.0.0", port=8080)
