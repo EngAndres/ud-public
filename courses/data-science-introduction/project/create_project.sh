7f9d35ebed2438d7ac45995afa5878a538fdfa5f
casierrav <jerarquia04@gmail.com>
2024-06-19 18:29:52 -0500

    Update sources

--- courses/data-science-introduction/project/create_project.sh
+++ courses/data-science-introduction/project/create_project.sh
@@ -15,9 +15,9 @@ poetry --version
 # create poetry project and add dependencies
 poetry new data-science-project
 cd data-science-project
-poetry add numpy pandas pandera pydantic tzdata openpyxl beaufitulsoup4 matplotlib seaborn ydata-profiling \
-psycopg2-binary sqlalchemy scikit-learn xgboost uvicorn fastapi jupyterlab
+poetry add numpy pandas pandera pydantic tzdata openpyxl beautifulsoup4 matplotlib seaborn psycopg2-binary sqlalchemy scikit-learn xgboost uvicorn fastapi jupyterlab
 poetry add -G dev black pylint pytest click pre-commit 
+pip install ydata-profiling
 
 # Restart shell
 exec "$SHELL"
