6c7b720b7bc10e4bbc690aec51f908229254b363
casierrav <jerarquia04@gmail.com>
2024-06-19 18:08:10 -0500

    Update install

---
+++ courses/data-science-introduction/project/create_project.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Create a VEnv for Python 3.11.8
+pyenv install 3.11.8
+pyenv virtualenv 3.11.8 data-science-introduction
+pyenv activate data-science-introduction
+
+# Install General Dependencies
+pip install --upgrade pip
+pip install ipython
+pip install poetry
+poetry config virtualenvs.in-project true
+poetry --version
+
+# create poetry project and add dependencies
+poetry new data-science-project
+cd data-science-project
+poetry add numpy pandas pandera pydantic tzdata openpyxl beaufitulsoup4 matplotlib seaborn ydata-profiling \
+psycopg2-binary sqlalchemy scikit-learn xgboost uvicorn fastapi jupyterlab
+poetry add -G dev black pylint pytest click pre-commit 
+
+# Restart shell
+exec "$SHELL"
