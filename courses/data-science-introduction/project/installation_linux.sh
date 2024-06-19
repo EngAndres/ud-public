dcc1949a18840407daebb1e05ee896cd01b2851b
casierrav <jerarquia04@gmail.com>
2024-06-19 18:08:10 -0500

    Update install

--- courses/data-science-introduction/project/installation_linux.sh
+++ courses/data-science-introduction/project/installation_linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Add the DBeaver CE repository
+# Add repositories, download files
 wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | sudo apt-key add -
 echo "deb https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
 wget https://downloads.slack-edge.com/desktop-releases/linux/x64/4.38.125/slack-desktop-4.38.125-amd64.deb
@@ -47,24 +47,6 @@ curl https://pyenv.run | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
-# Create a VEnv for Python 3.11.8
-curl -sSL https://install.python-poetry.org | bash
-echo 'export PATH="$HOME/.poetry/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
-poetry config virtualenvs.in-project true
-pyenv install 3.11.8
-pyenv virtualenv 3.11.8 data-science-introduction
-pyenv activate data-science-introduction
-
-# Install General Dependencies
-pip install --upgrade pip
-pip install ipython
-# create poetry project and add dependencies
-poetry new data-science-project
-cd data-science-project
-poetry add numpy pandas pandera pydantic tzdata openpyxl beaufitulsoup4 matplotlib seaborn ydata-profiling \
-psycopg2-binary sqlalchemy scikit-learn xgboost uvicorn fastapi jupyterlab
-poetry add -G dev black pylint pytest click pre-commit 
-
-# Restart shell
+pyenv --version
 exec "$SHELL"
