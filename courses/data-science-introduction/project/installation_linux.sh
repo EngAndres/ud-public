9050c1a80218729499f75c7c3ed3cb2ffe583762
casierrav <cavirguezs@udistrital.edu.co>
2024-06-20 18:39:37 -0500

    DataScience Introduction: update shells to install tools on linux

--- courses/data-science-introduction/project/installation_linux.sh
+++ courses/data-science-introduction/project/installation_linux.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Add repositories, download files
-wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | sudo apt-key add -
-echo "deb https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
+sudo add-apt-repository ppa:serge-rider/dbeaver-ce
 wget https://downloads.slack-edge.com/desktop-releases/linux/x64/4.38.125/slack-desktop-4.38.125-amd64.deb
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
@@ -14,15 +13,15 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 
 # Install prerequisites
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
-libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+sudo apt -y install build-essential cmake curl pkg-config libgtk-3-dev libgtkmm-3.0-dev liblensfun-dev librsvg2-dev liblcms2-dev libfftw3-dev libiptcdata0-dev libtiff5-dev libcanberra-gtk3-dev libexiv2-dev
+sudo apt -y install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+sudo apt-get install -y make libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev git
+sudo apt-get install -y apt-transport-https ca-certificates software-properties-common
 
 # Install Apache
 sudo apt-get install -y apache2
 # Install DBeaver
-sudo apt-get install -y dbeaver-ce
+sudo apt -y install dbeaver-ce
 # Install VSCode
 sudo dpkg -i vscode.deb
 rm vscode.deb
@@ -34,7 +33,7 @@ rm postman.tar.gz
 sudo dpkg -i slack-desktop-4.38.125-amd64.deb
 rm slack-desktop-4.38.125-amd64.deb
 # Install Docker
-sudo apt-get install -y docker-ce
+sudo apt-get install -y docker.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
