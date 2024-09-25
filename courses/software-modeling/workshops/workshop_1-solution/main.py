e5061e32a26e4c4d1be41d17d80503bc76e17239
casierrav <cavirguezs@udistrital.edu.co>
2024-09-25 14:03:08 -0500

    Software Modeling: codes updates in codes

--- courses/software-modeling/workshops/workshop_1-solution/main.py
+++ courses/software-modeling/workshops/workshop_1-solution/main.py
@@ -19,12 +19,15 @@ You should have received a copy of the GNU General Public License
 along with Workshop-SM-UD. If not, see <https://www.gnu.org/licenses/>. 
 """
 
+import sys
 import pickle
 from datetime import datetime
 from videogames import VideoGame
 from machines import Machine
 from users import User, Client, Manager, Address
 
+
+# pylint: disable=too-few-public-methods
 class Delivery:
     """This class represents the behavior of a delivery in the application."""
 
@@ -33,19 +36,25 @@ class Delivery:
         self.__address = address
         self.__machine = machine
 
+    def __str__(self) -> str:
+        return f"{'='*10}\nClient: {self.__client_info}\n\
+            Address: {self.__address}\nMachine: {self.__machine}"
+
+
 class Main:
     """This class represents the main behavior of the application."""
 
-    USER_TYPE = "1.Client\n2.Admin\n3.Exit"
     MENU_ADMIN = "1.Add Videogame\n2.Remove Videogame\n3.Exit"
-    MENU_CLIENT = (
-        "1.Choose Material\n2.Show VideoGames\n3.Add VideoGame to Machine\4.Buy Machine\n5.Exit"
-    )
+    MENU_CLIENT = "1.Choose Material\n2.Show VideoGames\n3.Add VideoGame to Machine\4.Buy Machine\n5.Exit"
 
-    def __init__(self):
+    def __init__(self, user: User):
         self.__catalog = []
         self.__temp_machine = None
-        self.__user : User = None
+        self.__user: User = user
+
+    def change_user(self, user: User):
+        """This method changes the current user."""
+        self.__user = user
 
     def __validate_videogame_code(self, code: int) -> bool:
         """This method validates if a videogame code already exists in the catalog.
@@ -78,69 +87,174 @@ class Main:
         else:
             print("You do not have permission to add a videogame.")
 
-    def remove_videogame(self, code: int):
+    def remove_videogame(self):
         """This method removes a videogame from the catalog.
 
         In this method based on videogame code, if the videogame
         exists it will be removed from current catalog.
-
-        Args:
-            code (int): Code of the videogame to be removed.
         """
-        success = False
-        response = self.__validate_videogame_code(code)
-        if response is not None:
-            self.__catalog.pop(response[0])
-            success = True
+        if self.__user.validate_grants("remove_videogames"):
+            success = False
+            code = int(input("Insert the code of the videogame:"))
+            response = self.__validate_videogame_code(code)
+            if response is not None:
+                self.__catalog.pop(response[0])
+                success = True
 
-        if success:
-            print("Videogame removed successfully.")
+            if success:
+                print("Videogame removed successfully.")
+            else:
+                print(f"Videogame with code {code} is not in the catalog.")
         else:
-            print(f"Videogame with code {code} is not in the catalog.")
+            print("You do not have permission to remove a videogame.")
 
     def choose_material(self):
         """This method allows to choose the material of the machine."""
-        material = input("Insert the material of the machine (wood, aluminum or carbon fiber):")
-        self.__temp_machine = Machine(material)
+        if self.__user.validate_grants("buy_machine"):
+            material = input(
+                "Insert the material of the machine (wood, aluminum or carbon fiber):"
+            )
+            self.__temp_machine = Machine(material)
+        else:
+            print("You do not have permission to choose a material.")
 
     def add_videogame_to_machine(self):
         """This method adds a videogame to the machine."""
-        code = int(input("Insert the code of the videogame:"))
-        response = self.__validate_videogame_code(code)
-        if response is not None:
-            self.__temp_machine.add_videogame(response[1])
-            print("Videogame added successfully.")
+        if self.__user.validate_grants("buy_machine"):
+            code = int(input("Insert the code of the videogame:"))
+            response = self.__validate_videogame_code(code)
+            if response is not None:
+                self.__temp_machine.add_videogame(response[1])
+                print("Videogame added successfully.")
+            else:
+                print("The videogame is not in the catalog.")
         else:
-            print("The videogame is not in the catalog.")
+            print("You do not have permission to add a videogame.")
 
     def show_videogames(self):
         """This method shows all videogames in the catalog."""
         for vg in self.__catalog:
             print(vg)
 
-    def get_delivery_information(self):
+    def __get_delivery_information(self):
         """This method gets the delivery information."""
         print("Choose delivery address:")
         for i, address in enumerate(self.__user.get_addresses()):
             print(f"{i+1}. {address}")
         option = int(input())
-        temp_address = self.__user.get_addresses()[option-1]
+        temp_address = self.__user.get_addresses()[option - 1]
         delivery = Delivery(self.__user, temp_address, self.__temp_machine)
 
         file_name = f"delivery_{datetime.now().strftime('%Y-%m-%d_%H%M%S')}_\
             {self.__user.get_id()}.pkl"
-        with open(file_name, 'wb') as file:
+        with open(file_name, "wb") as file:
             pickle.dump(delivery, file)
 
-        print("Delivery will be sent to:", self.__user.get_addresses()[option-1])
+        print("Delivery will be sent to:", self.__user.get_addresses()[option - 1])
 
     def buy_machine(self):
         """This method buys the machine."""
-        if self.__temp_machine is not None:
-            if isinstance(self.__user, Client) and (self.__user.validate_grant("buy_machine")):
-                self.get_delivery_information()
+        if isinstance(self.__user, Client) and (
+            self.__user.validate_grant("buy_machine")
+        ):
+            if self.__temp_machine is not None:
+                self.__get_delivery_information()
                 self.__temp_machine = None
             else:
-                print("You do not have permission to buy a machine.")
+                print("You must choose a material first and add videogames.")
         else:
-            print("You must choose a material first and add videogames.")
+            print("You do not have permission to buy a machine.")
+
+    def show_menu(self):
+        """This method shows the menu according to the user type."""
+        if isinstance(self.__user, Manager):
+            print(Main.MENU_ADMIN)
+        elif isinstance(self.__user, Client):
+            print(Main.MENU_CLIENT)
+
+    def __handle_admin(self, option: int):
+        exit_ = False
+        if option == 1:  # add videogame
+            self.add_videogame()
+        elif option == 2:  # remove videogame
+            self.remove_videogame()
+        elif option == 3:  # exit
+            print("Manager view is closing!")
+            exit_ = True
+
+        return exit_
+
+    def __handle_client(self, option: int) -> bool:
+        exit_ = False
+        if option == 1:  # choose material
+            self.choose_material()
+        elif option == 2:  # show videogames
+            self.show_videogames()
+        elif option == 3:  # add videogame to machine
+            self.add_videogame_to_machine()
+        elif option == 4:  # buy machine
+            self.buy_machine()
+        elif option == 5:  # exit
+            print("Client view is closing!")
+            exit_ = True
+
+        return exit_
+
+    def handle_option(self, option: int) -> bool:
+        """This method handles the option selected by the user.
+
+        Args:
+            option (int): Option selected by the user.
+        """
+        if isinstance(self.__user, Manager):
+            exit_ = self.__handle_admin(option)
+        elif isinstance(self.__user, Client):
+            exit_ = self.__handle_client(option)
+        return exit_
+
+
+# ======================= main execution ======================= #
+
+
+def get_user() -> User:
+    """This function gets the user type."""
+    options = "1.Manager/n2.Client/n3.Exit"
+    type_user = int(input(options))
+
+    while type_user not in [1, 2, 3]:
+        print("Invalid option. Please try again.")
+        type_user = int(input(options))
+
+    user = None
+    if type_user == 1:
+        user = Manager(1, "admin", "admin@udistrital.edu.co")
+    elif type_user == 2:
+        address = Address("St. Evergreen 123", 110783, "Springfield", "USA")
+        user = Client(
+            2, "client", "client@udistrital.edu.co", "+57 321 456789123", address
+        )
+    elif type_user == 3:
+        print("Thanks for using the application. Goodbye!")
+        sys.exit()
+
+    return user
+
+
+def run():
+    """This function runs the application."""
+    user = get_user()
+    main = Main(user)
+
+    while True:
+        while True:
+            main.show_menu()
+            option = int(input("Enter the option:"))
+            if main.handle_option(option):
+                break
+
+        user = get_user()
+        main.change_user(user)
+
+
+if __name__ == "__main__":
+    run()
