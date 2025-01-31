9315383fec6f3518dfabc22e3d6953cb509fe16c
Carlos Andres Sierra <cavirguezs@udistrital.edu.co>
2025-01-31 14:11:47 -0500

    Update: Project Code

---
+++ courses/software-modeling/project/java_services/src/main/java/ud/sm_project/videogames_machines/VideogamesMachinesApplication.java
@@ -0,0 +1,13 @@
+package ud.sm_project.videogames_machines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideogamesMachinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideogamesMachinesApplication.class, args);
+	}
+
+}
--- courses/software-modeling/project/python_services/backendpython/controllers/videogames.py
+++ courses/software-modeling/project/python_services/backendpython/controllers/videogames.py
@@ -12,50 +12,51 @@ router = APIRouter()
 
 services = VideoGameServices()
 
-@router.get("/videogames/all")        
-def get_all() -> List[VideoGamesDAO]:
-    """This method is used to get all videogames.
-    """
-    return services.get_all() 
 
-@router.get("/videogames/by_name/{name}")    
+@router.get("/videogames/all")
+def get_all() -> List[VideoGamesDAO]:
+    """This method is used to get all videogames."""
+    return services.get_all()
+
+
+@router.get("/videogames/by_name/{name}")
 def get_by_name(name: str) -> List[VideoGamesDAO]:
-    """This method is used to get videogames by name.
-    """
+    """This method is used to get videogames by name."""
     if name == "":
-        raise HTTPException(status_code=400, detail="The name cannot be empty.") 
+        raise HTTPException(status_code=400, detail="The name cannot be empty.")
     return services.get_by_name(name)
 
+
 def get_by_category(category: str) -> List[VideoGamesDAO]:
-    """This method is used to get videogames by category.
-    """
-    if category != "":
-        return services.get_by_category(category)
-    else:
+    """This method is used to get videogames by category."""
+    if category == "":
         raise HTTPException(status_code=400, detail="The category cannot be empty.")
+    return services.get_by_category(category)
+
 
 def get_by_price(min_price: float, max_price: float) -> List[VideoGamesDAO]:
-    """This method is used to get videogames by price.
-    """
-    if min_price < max_price:
-        return services.get_by_price(min_price, max_price)
-    else:
-        raise HTTPException(status_code=400, detail="The minimum price must be less than the maximum price.")
+    """This method is used to get videogames by price."""
+    if min_price >= max_price:
+        raise HTTPException(
+            status_code=400,
+            detail="The minimum price must be less than the maximum price.",
+        )
+    return services.get_by_price(min_price, max_price)
 
-def get_by_description(self, keyword: str) -> List[VideoGamesDAO]:
-    """This method is used to get videogames by description.
-    """
-    if keyword != "":
-        return services.get_by_description(keyword)
-    else:
+
+def get_by_description(keyword: str) -> List[VideoGamesDAO]:
+    """This method is used to get videogames by description."""
+    if keyword == "":
         raise HTTPException(status_code=400, detail="The keyword cannot be empty.")
+    return services.get_by_description(keyword)
 
-def get_by_name_category(self, data: NameCategoryDTO) -> List[VideoGamesDAO]:
+
+def get_by_name_category(data: NameCategoryDTO) -> List[VideoGamesDAO]:
     """This method is used to get videogames by name and category.
     NameCategoryDTO is expected to have name and category as fields.
     """
-    if data.name != "" and data.category != "":
-        return services.get_by_name_category(data)
-    else:
-        raise HTTPException(status_code=400, detail="The name and category cannot be empty.")
-    
+    if data.name == "" or data.category == "":
+        raise HTTPException(
+            status_code=400, detail="The name and category cannot be empty."
+        )
+    return services.get_by_name_category(data)
