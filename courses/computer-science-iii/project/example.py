a266f96da54a0bb22e5bd0429cd824fb2dd1ef9d
Carlos Andres Sierra <cavirguezs@udistrital.edu.co>
2025-02-12 02:10:55 -0500

    CS: project compiler example update

--- courses/computer-science-iii/project/example.py
+++ courses/computer-science-iii/project/example.py
@@ -2,26 +2,30 @@
 
 from compiler import Compiler
 
+
 # =========== Example usage ========== #
 def example1(compiler_: Compiler):
     """This function is an example of how to use the compiler."""
     input_text = """
     START 
+    TIME 4/4
     Cb5 1/2 F5 1/4 Ab5 1/4 Bb5 1/4 Bb5 1/8 Ab5 1 Fb5 1/8 As5 1/4 Cb5 1/16 As5 1/16
     END
     """
     compiler_.compile(input_text)
 
+
 def example2(compiler_: Compiler):
     """This function is an example of how to use the compiler."""
     input_text = """
     START 
-    C4 1/4 D4 1/4 E4 1/4 F4 1/4 G4 1/4 A4 1/4 B4 1/4 C5 1/4 
+    TIME 3/4
+    C4 1/4 D4 1/4 E4 1/4 F4 1/4 G4 1/4 A4 1/4 B4 1/4 C5 1/4 B4 1/4 A4 1/4 G4 1/4 F4 1/4 E4 1/4 D4 1/4 C4 1/4
     END
     """
     compiler_.compile(input_text)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     compiler = Compiler()
-    example2(compiler)
+    example1(compiler)
