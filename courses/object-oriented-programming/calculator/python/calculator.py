acea588ea0a6ec3d728e4e04afb69775c19c3be3
casierrav <jerarquia04@gmail.com>
2025-04-11 15:49:12 -0500

    OOP codes

---
+++ courses/object-oriented-programming/calculator/python/calculator.py
@@ -0,0 +1,56 @@
+class Calc:
+    """This is an example of a calculator as an object"""
+
+    def __init__(self):
+        self.memory = 0.0
+
+    def sum (self, a1: float, b2: float) -> float:
+        """This method makes an addition to another number.
+        
+        Args:
+            a1(float): First number in the addition
+            b2(float): Second number in the addition
+
+        Returns:
+            The sum of the two numbers.
+        """
+        return a1 + b2
+    
+    def substract(self, num1: float, num2: float) -> float:
+        """
+        This method makes a substraction of the second
+        number from the first number.
+        
+        Args:
+            num1(float): First number of the substraction
+            num2(float): Second number of the substraction
+
+        Returns:
+            The result of the substraction.
+        """    
+        return num1 - num2
+    
+    def multiplication(self, num1: float, num2: float) -> float:
+        """
+        This method multiplies two numbers.
+
+        Args:
+            num1(float): First number of the multiplication
+            num2(float): Second number of the multiplication
+
+        Returns:
+            The result of the multiplication
+        """
+        return num1 * num2
+    
+    def division(self, num1: float, num2: float) -> float:
+        """
+        This method makes the division of two numbers.
+        
+        Args:
+            num1(float): Enumerator of the division
+            num2(float): Denominator of the division
+
+        Returns:
+            The result of the division.
+        """
