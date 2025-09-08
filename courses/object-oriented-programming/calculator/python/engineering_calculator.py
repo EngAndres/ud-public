299d547a9438f4dc90013cb2628d99af48acecfe
Carlos Andres Sierra <cavirguezs@udistrital.edu.co>
2025-09-08 00:44:01 -0500

    OOP course update

--- courses/object-oriented-programming/calculator/python/engineering_calculator.py
+++
@@ -1,9 +0,0 @@
-from scientific_calculator import ScientificCalculator
-
-class EngineeringCalculator(ScientificCalculator):
-    pass
-
-
-test_eng = EngineeringCalculator()
-print(test_eng.sum(3,4))
-print(test_eng.square(5))
