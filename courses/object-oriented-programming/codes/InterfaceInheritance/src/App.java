374f7fba04de9c1ad84bf6111888b19ebb7ddea6
Andres Sierra <jerarquia04@gmail.com>
2025-10-24 17:55:47 -0500

    OOP inheritance exercise

--- courses/object-oriented-programming/codes/InterfaceInheritance/src/App.java
+++ courses/object-oriented-programming/codes/InterfaceInheritance/src/App.java
@@ -1,43 +1,86 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class App {
+
+    public static Character getCharacter(int option){
+        Character c = null;
+        switch(option){
+            case 1:
+                c = new Kitana("Kitana");
+                break;
+            case 2:
+                c = new LiuKahn("Liu Kahn", 20, 12);
+                break;
+            case 3:
+                c = new Sonia("Sonia", 15, 15); 
+                break; 
+        }
+        return c;
+    }
+
+    public static ArrayList<Character> chooseCharacters(){
+        int action = -1;
+        String options = "1. Kitana\t2. Liu Kahn\t3.Sonia";
+        
+        ArrayList<Character> characters = new ArrayList<Character>();
+        Scanner sc = new Scanner(System.in);
+
+        // Choose Player 1
+        do{
+            System.out.println("Choose Player 1:\n" + options);
+            action = sc.nextInt();
+        }while(action < 1 || action > 3);
+        characters.add( getCharacter(action) );
+
+        // Choose Player 2
+        do{
+            System.out.println("Choose Player 2:\n" + options);
+            action = sc.nextInt();
+        }while(action < 1 && action > 3);
+        characters.add( getCharacter(action) );
+
+        return characters;
+    }
 
     public static int chooseAction(String characterName){      
         int action = -1;
         Scanner sc = new Scanner(System.in);
         do{
-            System.out.println("Choose action for " + characterName 
-                    + ":\n1. Punch\n2: Kick\n");
+            System.out.println("\nChoose action for " + characterName 
+                    + ":\n1. Punch\n2: Kick");
             action = Integer.parseInt(sc.next());
-        }while(action < 1 && action > 2);
+        }while(action < 1 || action > 2);
         return action;
     }
 
-    public static void main(String[] args) throws Exception {
-        Kitana kitana = new Kitana();
-        LiuKahn liuKahn = new LiuKahn(18, 10);
-
+    public static void main(String[] args) {
         boolean gameOver = false;
+        int turn = 0; 
+        int nextTurn = 1;
+
+        ArrayList<Character> characters = chooseCharacters();
 
         while(!gameOver){
-            if(chooseAction("Kitana") == 1){
-                liuKahn.sufferDamage(kitana.launchPunch());
+            if(chooseAction(characters.get(turn).name) == 1){
+                characters.get(nextTurn).sufferDamage( characters.get(turn).launchPunch());
             } else {
-                liuKahn.sufferDamage(kitana.launchKick());
+                characters.get(nextTurn).sufferDamage( characters.get(turn).launchKick());
             }
-            kitana.sufferDamage(liuKahn.launchKick());
-            
-            gameOver = kitana.isDead();
+            gameOver = characters.get(nextTurn).isDead();
 
-            System.out.println("Both characters are alive. " + gameOver);
-
-            kitana.sufferDamage(liuKahn.launchKick());
-            kitana.sufferDamage(liuKahn.launchKick());
-            kitana.sufferDamage(liuKahn.launchKick());
-            kitana.sufferDamage(liuKahn.launchKick());
-         
-            gameOver = kitana.isDead();
-            System.out.println("Massive attack over kitana. " + gameOver);
+            if(gameOver){
+                System.out.println(characters.get(nextTurn).name + 
+                    " is dead. " + characters.get(turn).name + 
+                    " wins.");
+            } else {
+                if(turn == 1){
+                    turn = 0;
+                    nextTurn = 1;
+                } else {
+                    turn = 1;
+                    nextTurn = 0;
+                }
+            }
         }
     }
 }
