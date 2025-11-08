7ac1cf00d1de8c0931c383676297135a24913852
Andres Sierra <jerarquia04@gmail.com>
2025-11-08 15:41:33 -0500

    UD classes update

---
+++ courses/object-oriented-programming/codes/pokemon/src/PokemonFeatures.java
@@ -0,0 +1,17 @@
+public class PokemonFeatures {
+    
+    protected int attackDamage;
+    protected int baseDefense;
+    protected int health;
+
+    public PokemonFeatures(int attackDamage, int baseDefense){
+        this.attackDamage = attackDamage;
+        this.baseDefense = baseDefense;
+        this.health = 100;
+    }
+
+    public boolean isDefeated(){
+        int minimumLevel = 5;
+        return this.health < minimumLevel;
+    }
+}
