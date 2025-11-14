8d02d7f83592c264059b8282842bf37e2279480e
Andres Sierra <jerarquia04@gmail.com>
2025-11-14 18:06:16 -0500

    OOP: Pokemon game update

---
+++ courses/object-oriented-programming/codes/pokemon/src/Pokedex.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Pokedex {
+    private ArrayList<Pokemon> pokemones;
+
+    public Pokedex(){
+        this.pokemones = new ArrayList<>();
+    }
+
+    public boolean addPokemon(Pokemon newPokemon){
+        return true;
+    }
+}
--- courses/object-oriented-programming/codes/pokemon/src/RockPokemon.java
+++ courses/object-oriented-programming/codes/pokemon/src/RockPokemon.java
@@ -1,3 +1,26 @@
-public class RockPokemon implements Pokemon {
-    
+public class RockPokemon extends PokemonFeatures implements Pokemon {
+
+    public RockPokemon(int attackDamage, int baseDefense){
+        super(attackDamage, baseDefense);
+    }
+
+    @Override
+    public int attack() {
+        return this.attackDamage;
+    }
+
+    @Override
+    public Pokemon evolute() {
+        return null;
+    }
+
+    @Override
+    public void defense(String typeAdversary, int attackValue) {
+        this.health -= attackValue;
+    }
+
+    @Override
+    public void healthRecovery() {
+        this.health += (int)((5 * this.health) / 100.0);
+    }
 }
