c28fa6fced4e2f0c7c9d7b8f34af5e762419ac6d
Andres Sierra <jerarquia04@gmail.com>
2025-11-26 20:37:50 -0500

    Classes update

---
+++ courses/software_engineering_seminar/project/python_backend/tests/test_calculator_division.py
@@ -0,0 +1,11 @@
+import pytest
+
+class TestCalculatorDivision:
+
+    def before_all(self):
+        # create object
+        pass
+
+    def before(self):
+        # before each unit test
+        pass
