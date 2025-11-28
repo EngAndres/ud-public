03ba15dfa6e7a2c7f720f39ae9efd0b70ca71c4c
Andres Sierra <jerarquia04@gmail.com>
2025-11-27 22:12:15 -0500

    OOP update

---
+++ courses/object-oriented-programming/codes/serialization/src/App.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class App {
+    
+    public static void serializeStudent(Student student, String filename){
+        try(){
+
+        } catch(){
+
+        }
+    }
+
+    public static void deserializeStudent(){
+
+    }
+    
+    public static void main(String[] args) throws Exception {
+        Student s = new Student("Pepita", 1234, "pepita@ud.edu.co");
+
+        System.out.println(s);
+    }
+}
