4f57408d5191e273eccab6ae22e9caffb8a9d5d5
Andres Sierra <jerarquia04@gmail.com>
2025-12-02 20:09:03 -0500

    Update SSE

---
+++ courses/software_engineering_seminar/project/java_backend/calculator_authentication/src/main/java/com/example/calculator_authentication/CalculatorAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculator_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorAuthenticationApplication.class, args);
+	}
+
+}
---
+++ courses/software_engineering_seminar/project/java_backend/calculator_authentication/src/test/java/com/example/calculator_authentication/CalculatorAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculator_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
